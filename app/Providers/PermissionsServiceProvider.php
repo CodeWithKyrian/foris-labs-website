6f8d9e4b7f5a592420ff0777749e9e6ad49378cd
CodeWithKyrian <koshnawaza@gmail.com>
2021-08-24 01:34:12 +0100

    jj

--- app/Providers/PermissionsServiceProvider.php
+++ app/Providers/PermissionsServiceProvider.php
@@ -16,12 +16,6 @@ class PermissionsServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        // Blade::directive('role', function ($role) {
-        //     return "<?php if(auth()->check() && auth()->user()->hasRole({$role})) :";
-        // });
-        // Blade::directive('endrole', function ($role) {
-        //     return "<?php endif; ?>";
-        // });
     }
 
     /**
