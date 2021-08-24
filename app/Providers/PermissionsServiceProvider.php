20b3a576fec66eef7a77af2d760c0a7708a16b85
CodeWithKyrian <koshnawaza@gmail.com>
2021-08-24 01:42:54 +0100

    remove permissions service

--- app/Providers/PermissionsServiceProvider.php
+++ app/Providers/PermissionsServiceProvider.php
@@ -25,10 +25,5 @@ class PermissionsServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Permission::get()->map(function ($permission) {
-            Gate::define($permission->slug, function ($user) use ($permission) {
-                return $user->hasPermissionTo($permission);
-            });
-        });
     }
 }
